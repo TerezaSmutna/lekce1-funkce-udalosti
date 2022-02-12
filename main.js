a4ffcc22a0992424d9b1a2ea6fe8aa22ba400ce5
LAPTOP-5LG8T5MU\terka <terka.kop@seznam.cz>
2022-02-12 14:58:25 +0100

    First lesson done

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // tady je místo pro náš program
-
+function zobraz(){
 document.querySelector("#vysledek").innerHTML = secti(4, 5);
-
+}
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
  *
@@ -18,4 +18,11 @@ function secti(a, b) {
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log("Gratulace, právě jsi spustila tuto funkci!");
+  document.querySelector(".ctverecek").textContent = 'Gratulace';
 }
+
+function zmenabarvy() {
+  document.querySelector(".ctverecek").style.backgroundColor = 'green';
+}
+let jmeno = 'jana';
